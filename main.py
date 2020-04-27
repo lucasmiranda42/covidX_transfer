506cce7a71595d395011b272189f4a20cc4f1f3a
lucas_miranda <lucasmiranda42@gmail.com>
2020-04-27 16:15:19 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ Main training pipeline for the covidX_transfer project
 
 '''
 
-#Load data using generators
+# Load data using generators
 
-#Load hypermodel and run hyperparameter tuning
+# Load hypermodel and run hyperparameter tuning
 
-#Train and deploy the resulting model
+# Train and deploy the resulting model
+
+# Explore model blending
+# https://www.kaggle.com/cdeotte/25-million-images-0-99757-mnist
