fdd05217a8960334e3e784865835d07d1589fed6
lucas_miranda <lucasmiranda42@gmail.com>
2020-04-29 20:21:47 +0200

    Implemented data generators and a naive model for testing

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ Main training pipeline for the covidX_transfer project
 
 import argparse
 from keras_preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
+from tensorflow.keras import layers, Model
 from tensorflow.keras.applications.nasnet import NASNetLarge
+from tensorflow.keras.optimizers import Adam
 
 parser = argparse.ArgumentParser(
     description="Training script for the covidX_transfer project"
@@ -35,7 +36,7 @@ verb = int(args.verbose)
 blend = args.blend
 
 if not path:
-    raise ValueError("set a valid path for the training to run")
+    raise ValueError("set a valid data path for the training to run")
 if verb not in [0, 1, 2]:
     raise ValueError("verbose has to be one of 0, 1 or 2")
 
@@ -44,6 +45,9 @@ pre_trained_model = NASNetLarge(
     input_shape=(331, 331, 3), include_top=False, weights="imagenet"
 )
 
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
 if verb == 2:
     print(pre_trained_model.summary())
 
@@ -51,8 +55,64 @@ datagen = ImageDataGenerator(
     rotation_range=10, zoom_range=0.10, width_shift_range=0.1, height_shift_range=0.1
 )
 
+last_layer = pre_trained_model.get_layer(pre_trained_model.layers[-1].name)
+last_output = last_layer.output
 
-# Load data using generators
+"""toy model for testing the generators"""
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation="relu")(x)
+# Add a dropout rate of 0.2
+x = layers.Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(3, activation="softmax")(x)
+
+model = Model(pre_trained_model.input, x)
+
+model.compile(
+    optimizer=Adam(lr=0.0001), loss="binary_crossentropy", metrics=["accuracy"]
+)
+
+if verb == 2:
+    print(model.summary())
+
+train_dir = "{}/train".format(path)
+test_dir = "{}/test".format(path)
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255.0,
+    rotation_range=10,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=False,
+)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    train_dir, batch_size=32, class_mode="categorical", target_size=(331, 331)
+)
+
+# Flow validation images in batches of 20 using test_datagen generator
+test_generator = test_datagen.flow_from_directory(
+    test_dir, batch_size=32, class_mode="categorical", target_size=(331, 331)
+)
+
+history = model.fit(
+    train_generator,
+    validation_data=test_generator,
+    steps_per_epoch=100,
+    epochs=1,
+    validation_steps=50,
+    verbose=verb,
+)
 
 # Load hypermodel and run hyperparameter tuning
 
