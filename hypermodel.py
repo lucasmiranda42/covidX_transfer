555c9b5028dc62bf200bee1f7f8a61ee3df510f6
lucas_miranda <lucasmiranda42@gmail.com>
2020-05-13 13:11:33 +0200

    Save all models

--- hypermodel.py
+++ hypermodel.py
@@ -123,7 +123,7 @@ def tune_search(train, test, fine_tune, project_name, verb):
     if verb == 2:
         print(tuner.results_summary())
 
-    return tuner.get_best_models(num_models=10)
+    return tuner.get_best_models()
 
 
 ### TODO:
